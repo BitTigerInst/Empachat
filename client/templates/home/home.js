bb88c3c85781c34cf841c3723b3e8a79a0a1d3d6
kaiqianz <zhangkaiqiang@zhangkaiqiangdeMacBook-Pro.local>
2016-02-29 01:13:26 -0800

    haha

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,10 +1,13 @@
 Messages = new Meteor.Collection('messages');
 Channels = new Meteor.Collection('channels');
 ChatRooms = new Meteor.Collection("chatrooms");
+Notifications = new Meteor.Collection('notifications');
 
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
+
+
 
 Meteor.startup(function(){
   SC.initialize({
@@ -15,8 +18,8 @@ Meteor.startup(function(){
 
 Template.home.events({
   'submit .search-emotion': function(event){
-    event.preventDefault();
     Router.go('/chat');
+    event.preventDefault();
     var emotion = event.target.emotion.value;
     SC.get('/tracks', {
       tag_list: emotion,
