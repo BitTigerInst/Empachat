7e033e64982abc24f23ea6a70b54c38f96898dba
YingjieHe <heyingjie1993@outlook.com>
2016-02-29 00:07:37 -0800

    merged master

--- client/templates/chatwindow/chatwindow.js
+++ client/templates/chatwindow/chatwindow.js
@@ -18,6 +18,13 @@ Template.input.events ({
               createdAt: Date.now()
             }}});
             console.log(de);
+            Notifications.insert({
+              postId: Meteor.userId(),
+              userId: Session.get("currentId"),
+              username: Meteor.user().username,
+              read: false
+            });
+            //console.log(Notifications.findOne(Meteor.userId());
             document.getElementById('message').value = '';
             message.value = '';
           }
