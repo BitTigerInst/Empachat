13c11f21ee9cc7a2f25e9b7f36535c952076f8d3
kaiqianz <zhangkaiqiang@zhangkaiqiangdeMacBook-Pro.local>
2016-02-29 00:51:12 -0800

    haha

---
+++ templates/chatwindow/chatwindow.js
@@ -0,0 +1,43 @@
+Tracker.autorun(function(){
+    Meteor.subscribe("chatrooms");
+});
+
+
+
+
+Template.input.events ({
+  'keydown input#message' : function (event) {
+    
+    if (event.which === 13) { 
+        if (Meteor.user())
+        {
+          var name = Meteor.user().username;
+          var message = document.getElementById('message');
+          if (message.value !== '') {
+              scrolldown();
+            var de = ChatRooms.update({"_id":Session.get("roomid")},{$push:{messages:{
+              name: name,
+              text: message.value,
+              createdAt: Date.now()
+              
+            }}});
+            console.log(de);
+            Notifications.insert({
+              postId: Meteor.userId(),
+              userId: Session.get("currentId"),
+              username: Meteor.user().username,
+              read: false
+            });
+            //console.log(Notifications.findOne(Meteor.userId());
+            document.getElementById('message').value = '';
+            message.value = '';
+            
+          }
+        }
+        else
+        {
+           alert("login to chat");
+        } 
+    }
+  }
+});
