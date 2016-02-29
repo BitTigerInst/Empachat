0f1f960874fb2f5bc9244c987c3aaa5db33ceb77
Jay <jay@jay-virtual-machine.(none)>
2016-02-29 00:07:37 -0800

    merged master

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,13 +1,23 @@
 Messages = new Meteor.Collection('messages');
 Channels = new Meteor.Collection('channels');
 ChatRooms = new Meteor.Collection("chatrooms");
+Notifications = new Meteor.Collection('notifications');
 
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
 
+
+Meteor.startup(function(){
+  SC.initialize({
+    client_id: 'd3f2b79d4e0732d66a4cc3accf02dd92'
+  });
+});
+
+
 Template.home.events({
-  "submit .search-emotion": function(event){
+  'submit .search-emotion': function(event){
+    Router.go('/chat');
     event.preventDefault();
     var emotion = event.target.emotion.value;
     //*************//
